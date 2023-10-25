2af1b85fffc4669997943c1617baaf3509913cf9
Rodrigoaaalves <136371023+Rodrigoaaalves@users.noreply.github.com>
2023-10-25 15:53:17 -0300

    Endereco.java

---
+++ Endereco.java
@@ -0,0 +1,29 @@
+public class Endereco {
+    private String rua;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+    public Endereco(String rua, String cidade, String estado, String cep) {
+        this.rua = rua;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getCEP() {
+        return cep;
+    }
+}
